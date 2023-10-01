fac312573666664af0c1de97fb76e5f2f61646f6
manzil-infinity180 <rahulvs2809@gmail.com>
2023-10-01 11:23:24 +0530

    userschema added

---
+++ model/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema =new mongoose.Schema({
+  email:{
+    type:String,
+    trim:true,
+    lowercase:true,
+    required:[true,'User cannot without emailId'],
+    match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
+  password:{
+    type:String,
+    required:[true,'Without password you can not signup/login']
+  },
+  confirmPassword:{
+    type:String,
+    required:[true,'Without password you can not signup/login'],
+    validate:{
+      validator:function(el){
+        return (el==this.password);
+      },
+      message:"Confirm Password must be same as Password"
+    }
+  }
+});
+
+const User = require('User',userSchema);
+module.exports=User;
