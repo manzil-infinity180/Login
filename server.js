0fedd02648d58c143e494870c3521e021d6a4c9f
manzil-infinity180 <rahulvs2809@gmail.com>
2024-07-21 18:25:00 +0530

    commiting .env.vault for ci

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const PORT = 8080 || process.env.PORT;
-dotenv.config({path: './config.env'}); 
+dotenv.config({path: './.env'});
 const app = require('./app'); // from app.js
 const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.PASSWORD);
 
